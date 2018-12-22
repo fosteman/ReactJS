cf47d9c047d3819dff907d5a0be35b6180be151e
Tim Fosteman <fosteman@mk8.local>
2018-12-22 08:51:51 -0500

    basic testing

--- testing/basics/Modash.js
+++ testing/basics/Modash.js
@@ -1,1 +1,22 @@
-// We write the Modash library in this file in the Unit Testing chapter
+function truncate(string, length) {
+    if (string.length > length)
+        return string.slice(0, length) + '...';
+    else
+        return string;
+}
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+function camelCase(string) {
+    let words = string.split(/[\s|\-|_]+/);
+    return [
+        words[0].toLowerCase(),
+        ...words.slice(1).map((word) => capitalize(word)),
+    ].join('');
+}
+const Modash = {
+  truncate,
+  capitalize,
+  camelCase,
+};
+export default Modash;
