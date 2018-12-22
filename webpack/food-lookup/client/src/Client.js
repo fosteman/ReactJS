e99b36d482309f7fefbab32d62899138540aef6d
Tim Fosteman <fosteman@mk8.local>
2018-12-22 07:34:35 -0500

    Webpack: API

--- webpack/food-lookup/client/src/Client.js
+++ webpack/food-lookup/client/src/Client.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-undef */
 function search(query, cb) {
-  return fetch(`http://localhost:3001/api/food?q=${query}`, {
-    accept: 'application/json',
-  }).then(checkStatus)
-    .then(parseJSON)
-    .then(cb);
+  return fetch(`/api/food?q=${query}`,
+      {
+        accept: 'application/json'
+      }
+      )
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(cb);
 }
 
 function checkStatus(response) {
