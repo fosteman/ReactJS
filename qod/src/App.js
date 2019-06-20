0c3f9f4cd70936729e93292b5916d0988be10e7a
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-06-20 07:12:52 -0400

    Quote Of the Day React component release!

--- qod/src/App.js
+++ qod/src/App.js
@@ -1,9 +1,23 @@
-import {useState} from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
+import axios from 'axios'
+let url = 'http://localhost:3001/';
 
-class default function() {
-  const [qod, setqod] = useState('');
+export default function() {
+  const [quotes, loadQuotes] = useState([]);
+  useEffect( () => {
+    const fetchData = async () => {
+      let t = await axios(url + 'data');
+      console.log(t.data);
+      loadQuotes(t.data);
+    };
+    fetchData();
+  }, []);
   return (
-      
+      <p id="qod">
+        {
+          quotes.length ? quotes[Math.floor(Math.random() * quotes.length)].quoteText : 'loading'
+        }
+      </p>
   );
 }
