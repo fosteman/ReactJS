84310bed0f9020448c72a526e8993516d4a4da14
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-08-16 15:36:48 -0400

    refactored into react hook

--- dialogflow-ui/src/App.js
+++ dialogflow-ui/src/App.js
@@ -1,48 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 import {Launcher} from 'react-chat-window';
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userMessage: '',
-      conversation: [],
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-
-  }
-
-  componentDidMount() {}
-
-  handleSubmit = messageObject => {
+export default function App() {
+  const handleSubmit = messageObject => {
     messageObject.author = 'User';
-
     this.setState({
       conversation: [...this.state.conversation, messageObject],
     });
-
-    axios('http://localhost:3010/chat', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        messageText: messageObject.data.text
-      }),
-    });
-
   };
-
-  render() {
-    return (
-          <Launcher
-              isOpen={true}
-              agentProfile={{teamName: "Fosteman's DialogFlow Chatbot"}}
-              onMessageWasSent={this.handleSubmit}
-              messageList={this.state.conversation}
-          />
-    );
-  }
+  return (
+        <Launcher
+            isOpen={true}
+            agentProfile={{teamName: "Fosteman's DialogFlow Chatbot"}}
+            onMessageWasSent={this.handleSubmit}
+            messageList={this.state.conversation}
+        />
+  );
 }
-
-export default App;
