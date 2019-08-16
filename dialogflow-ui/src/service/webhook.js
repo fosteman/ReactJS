c09e40239515439325835b3a7f6020c1cbf88d3f
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-08-16 15:23:26 -0400

    moved message processor function into the webhook

--- dialogflow-ui/src/service/webhook.js
+++ dialogflow-ui/src/service/webhook.js
@@ -3,18 +3,31 @@ require('dotenv').config({ path: 'variables.env' });
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const processMessage = require('./messageProcessor');
-
 const app = express();
+const {WebhookClient, Card, Suggestion} = require('dialogflow-fulfillment');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const processMessage = (request, respose) => {
+    const agent = new WebhookClient({ request, respose });
+    console.log('Dialogflow Request body: ' + JSON.stringify(agent));
+
+
+    function welcome(agent) {
+        agent.add(`Welcome to my agent!`);
+        agent.add(new Suggestion(`Example suggestion`));
+    }
+
+    let intentMap = new Map();
+    intentMap.set('Welcome', welcome);
+    agent.handleRequest(intentMap);
+};
 
 app.post('/', (req, res) => {
     console.log('chat post log', req.body);
-    processMessage(req.body);
+    processMessage(req, res);
 });
 
 app.set('port', process.env.PORT);
