93b15914a7c4c683d89a99cc4b655ade803551e6
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-08-20 06:42:48 -0400

    environment vars

--- dialogflow-ui/src/App.js
+++ dialogflow-ui/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import {Launcher} from 'react-chat-window';
 import dialogflow from 'dialogflow';
 import uuid from 'uuid';
-require('dotenv').config({path :'.env'});
+import env from './env';
+
+const private_key = env.REACT_APP_DF_PRIVATE_TOKEN;
+const client_email = env.REACT_APP_DF_CLIENT_EMAIL;
+const projectId = env.REACT_APP_DF_PROJECT_ID;
 
 export default function App() {
   const [conversation, newMessage] = React.useState([]);
@@ -14,27 +18,23 @@ export default function App() {
   async function handleSubmit(queryObject) {
     newMessage([...conversation, queryObject]);
 
-    const private_key = process.env.DF_PRIV_KEY;
-    const client_email = process.env.DF_CLIENT_EMAIL;
-    const projectId = process.env.DF_PROJECT_ID;
-
     const queryText = queryObject.data.text;
-    console.log(projectId, ' : ',queryText);
 
-    if (!projectId) return console.error('Dialogflow projectId not found in .env file!');
+    if (!projectId || !private_key) return console.error('Dialogflow variables not found in .env file!');
 
     // Create a unique identifier for the session
     const sessionId = uuid.v4();
     // Create a new session
-    const sessionsClient = new dialogflow.SessionsClient(
+    const sessionClient = new dialogflow.SessionsClient(
         {
           credentials: {
-            private_key
+            private_key,
             client_email
           },
         }
     );
-    const sessionPath = sessionsClient.sessionPath(projectId, sessionId);
+    //define session path
+    const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
     // The text query request.
     const request = {
@@ -48,19 +48,23 @@ export default function App() {
         },
       },
     };
-
-    // Send request and log result
-    const responses = await sessionPath.detectIntent(request);
-    console.log("Detected intent. Here's response:", responses);
-    // Get query result
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-    if (result.intent) {
-      console.log(`  Intent: ${result.intent.displayName}`);
-    } else {
-      console.log(`  No intent matched.`);
+    try {
+      let responses = await sessionClient.detectIntent(request);
+      const result = responses[0].queryResult;
+      console.log(`  Query: ${result.queryText}`);
+      console.log(`  Response: ${result.fulfillmentText}`);
+      if (result.intent) {
+        console.log(`  Intent: ${result.intent.displayName}`);
+      } else {
+        console.log(`  No intent matched.`);
+      }
+      return responses
+    } catch (err) {
+      console.error('DialogFlow.sendTextMessageToDialogFlow ERROR:', err);
+      throw err
     }
+    // Send request and log result
+    // Get query result
   }
 
   return (
